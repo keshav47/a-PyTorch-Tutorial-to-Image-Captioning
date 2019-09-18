b7ba4fc3b52648e0f7cceb3ecb132f33dc5e333d
Keshav <kshavgupta47@gmail.com>
2019-09-18 19:02:06 +0530

    aaa

--- create_input_files.py
+++ create_input_files.py
@@ -2,10 +2,10 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='/home/ubuntu/filestore/ms-coco/show-attend-tell/dataset_coco.json',
-                       image_folder='/home/ubuntu/filestore/ms-coco/',
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='/home/ubuntu/filestore/ms-coco/show-attend-tell/dataset_flickr8k.json',
+                       image_folder='/home/ubuntu/filestore/flickr8k/Flickr8k/Flicker8k_Dataset/',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/home/ubuntu/filestore/keshav/show-attend-tell/output/',
+                       output_folder='/home/ubuntu/filestore/keshav/show-attend-tell/output/flickr8k/',
                        max_len=50)
