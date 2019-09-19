cb788f191dafadd8adb2af91bb8a24fde7d29f6b
Keshav <kshavgupta47@gmail.com>
2019-09-19 15:51:39 +0530

    aaa

--- create_input_files.py
+++ create_input_files.py
@@ -2,7 +2,7 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='flickr8k',
+    create_input_files(dataset='fashion',
                        karpathy_json_path='/home/ubuntu/filestore/keshav/show-attend-tell/data/dataset_fashion.json',
                        image_folder='/home/ubuntu/filestore/fashion_gen_data/images/',
                        captions_per_image=1,
